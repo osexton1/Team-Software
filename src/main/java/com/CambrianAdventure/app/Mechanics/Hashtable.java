0376af02a72a2fbab807aa045e10b1ce5d88c057
Odhran Sexton <odhran.sexton@gmail.com>
2022-02-01 15:52:46 +0000

    Updated dictionary file

--- src/main/java/com/CambrianAdventure/app/Mechanics/Hashtable.java
+++ src/main/java/com/CambrianAdventure/app/Mechanics/Hashtable.java
@@ -3,12 +3,12 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-public class Hashtable<v,k> extends Dictionary<v,k> implements Map<v,k>, Cloneable, Serializable {
-    public Hashtable(){
+class My_Dictionaries {
+    public static void main(String[] args) {
+
         Hashtable<Integer, String> randdesc = new Hashtable<Integer, String>();
         randdesc.put(1, "You come across a flatbed within the coral. The clearing is teeming with small critters foraging for food in thew sediment.");
         randdesc.put(2, "The frosty waters have calmed slightly, but not to a point of comfort. The remains of behemoths that couldn't handle the temperatures litter the seabed.");
-
 
         Hashtable<Integer, String> Events = new Hashtable<Integer, String>();
         Events.put(0, "Event - Something feels different.");
