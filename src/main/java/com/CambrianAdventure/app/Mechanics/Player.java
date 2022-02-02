a77f2754f290318a4116c35ef93318fa114b7840
Andrew Barrett <andrewbarrett148@gmail.com>
2022-02-02 16:56:48 +0000

    food level check in player

--- src/main/java/com/CambrianAdventure/app/Mechanics/Player.java
+++ src/main/java/com/CambrianAdventure/app/Mechanics/Player.java
@@ -5,15 +5,23 @@ import java.util.Objects;
 
 public class Player {
     public int health;
-    public int food;
+    public Integer food;
     public int evolutionLevel;
     public Environment Current;
+    public String foodOutput;
+    public String foodStr;
 
     public Player(){
         health = 3;
+        //max of 3
         food = 20;
+        foodStr = "";
+        //default of 20, max of 50
         evolutionLevel = 0;
+        //default of 0, max not determined
         Current = new Shallows();
+        //default shallows
+        foodOutput = "";
     }
 
     public void Move(String input){
@@ -29,5 +37,17 @@ public class Player {
             Current = Current.leftPath;
             Current.LoadBiomes();
         }
+    }
+    public String FoodLvl(String input){
+        if (Objects.equals(input, "9")) {
+            foodStr = food.toString();
+            foodOutput += "You have "+foodStr+" food left.";
+            if (food >= 25) {
+                foodOutput += " Hunger pangs.";
+            } if (food <= 75) {
+                foodOutput += " You are feeling well fed.";
+            }
+        }
+        return foodOutput;
     }
 }
