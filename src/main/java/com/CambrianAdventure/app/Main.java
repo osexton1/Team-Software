e3eabac50239ff63cc0735e897129d7259f0d5fb
Andrew Barrett <andrewbarrett148@gmail.com>
2022-02-03 00:39:35 +0000

    few comments and additions
    changing to a different branch because home laptop wanted to change syntax a bunch, is probably fine but don't want to push to main yet jic it fucked with something I didn't catch while reading over it

--- src/main/java/com/CambrianAdventure/app/Main.java
+++ src/main/java/com/CambrianAdventure/app/Main.java
@@ -1,77 +1,82 @@
 package com.CambrianAdventure.app;
+
 import com.CambrianAdventure.app.Mechanics.*;
 import com.CambrianAdventure.app.Mechanics.Environments.*;
 import com.CambrianAdventure.app.exploration.Scenario;
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Player Char = new Player();
         Char.Current = new Shallows();
         Char.Current.LoadBiomes();
         Char.Current.LoadRoom();
-        while(true) {
-//            Char.Current.scenario.completed = true;
-//            System.out.println(Char.Current);
-//            pathBiomes(Char);
+        while (true) {
+            // Char.Current.scenario.completed = true;
+            // System.out.println(Char.Current);
+            // pathBiomes(Char);
 
-//            System.out.println(Char.Current.scenario);
+            // System.out.println(Char.Current.scenario);
             pathRooms(Char);
 
-//        String inputText = System.console().readLine();
+            // String inputText = System.console().readLine();
             Scanner myObj = new Scanner(System.in);
-            String inputText = myObj.nextLine();  // Read user input
+            String inputText = myObj.nextLine(); // Read user input
             Inputting(Char, inputText);
         }
     }
 
     public static void biomechangeDesc(Scenario room) {
-        MyDictionaries Dict = new MyDictionaries(); //hashtable
+        MyDictionaries Dict = new MyDictionaries(); // hashtable
         System.out.println(Dict.roomType.get(room.type));
-        System.out.println(Dict.NumPaths.get(room.type));
-    }
-//    note that it may not be necessary to split these methods. I'm just doing it this way at the moment
-//    because that fits better with how I made the hashtables.
-    public static void roomdesc(Scenario room){
-        MyDictionaries Dict = new MyDictionaries(); //Used for Hashtables
-        //random descriptor
-        System.out.println(Dict.randdesc.get(0)); //Used for Hashtables
         System.out.println(Dict.NumPaths.get(room.numPaths));
     }
 
-    public static void pathRooms(Player Char){
+    // note that it may not be necessary to split these methods. I'm just doing it
+    // this way at the moment
+    // because that fits better with how I made the hashtables.
+    public static void roomdesc(Scenario room) {
+        MyDictionaries Dict = new MyDictionaries();
+        System.out.println(Dict.randdesc.get(0));
+        if (room.completed) {
+            System.out.println(Dict.NumPaths.get(room.numPaths));
+            // if this should be elsewhere just move it
+        }
+
+    }
+
+    public static void pathRooms(Player Char) {
         String output = "Enter a number to move to a new location: (1. " + Char.Current.scenario.middlePath.Name;
         if (Char.Current.scenario.leftPath != null) {
             output += "/2. " + Char.Current.scenario.leftPath.Name;
         }
-        if (Char.Current.scenario.rightPath != null){
+        if (Char.Current.scenario.rightPath != null) {
             output += "/3. " + Char.Current.scenario.rightPath.Name;
         }
         output += ")";
         System.out.println(output);
     }
 
-    public static void pathBiomes(Player Char){
+    public static void pathBiomes(Player Char) {
         String output = "Enter a number to move to a new location: (1. " + Char.Current.middlePath.Name;
         if (Char.Current.leftPath != null) {
             output += "/2. " + Char.Current.leftPath.Name;
         }
-        if (Char.Current.rightPath != null){
+        if (Char.Current.rightPath != null) {
             output += "/3. " + Char.Current.rightPath.Name;
         }
         output += ")";
         System.out.println(output);
     }
 
-    public static void Inputting(Player Char, String input){
+    public static void Inputting(Player Char, String input) {
 
-        if (Char.Current.completed){
+        if (Char.Current.completed) {
             Char.Move(input, false);
         }
-        if (Char.Current.scenario.completed){
+        if (Char.Current.scenario.completed) {
             Char.Move(input, true);
-        }
-        else {
+        } else {
             System.out.println(input);
         }
     }
--- src/main/java/com/CambrianAdventure/app/Mechanics/Player.java
+++ src/main/java/com/CambrianAdventure/app/Mechanics/Player.java
@@ -1,4 +1,5 @@
 package com.CambrianAdventure.app.Mechanics;
+
 import java.util.Objects;
 
 public class Player {
@@ -6,47 +7,45 @@ public class Player {
     public Integer food;
     public Integer evolutionLevel;
     public Integer playerClass;
-    //this can just be an integer thinking about it as opposed to 3 seperate classes.
-    //0 = Shelled, 1 = Finned, 2 = Spiked
+    // this can just be an integer thinking about it as opposed to 3 seperate
+    // classes.
+    // 0 = Shelled, 1 = Finned, 2 = Spiked
     public Integer biomeCount = 1;
     public Integer roomCount = 1;
     public Integer globalRoomCount = 1;
 
     public Environment Current;
 
-    public Player(){
+    public Player() {
         health = 3;
-        //max of 3
-        //will never increase
+        // max of 3
+        // will never increase
         food = 20;
-        //default of 20, max of 50
+        // default of 20, max of 50
         evolutionLevel = 0;
         Current = null;
-        //default of 0, max not determined
+        // default of 0, max not determined
     }
 
-    public void Move(String input, boolean room){
-        if (room){
+    public void Move(String input, boolean room) {
+        if (room) {
             if (Objects.equals(input, "1")) {
                 Current.scenario = Current.scenario.middlePath;
                 Current.LoadRoom();
                 roomCount += 1;
                 globalRoomCount += 1;
-            }
-            else if (Objects.equals(input, "2") && Current.scenario.leftPath != null) {
+            } else if (Objects.equals(input, "2") && Current.scenario.leftPath != null) {
                 Current.scenario = Current.scenario.leftPath;
                 Current.LoadRoom();
                 roomCount += 1;
                 globalRoomCount += 1;
-            }
-            else if (Objects.equals(input, "3") && Current.scenario.rightPath != null) {
+            } else if (Objects.equals(input, "3") && Current.scenario.rightPath != null) {
                 Current.scenario = Current.scenario.rightPath;
                 Current.LoadRoom();
                 roomCount += 1;
                 globalRoomCount += 1;
             }
-        }
-        else {
+        } else {
             if (Objects.equals(input, "1")) {
                 Current = Current.middlePath;
                 Current.LoadBiomes();
@@ -69,9 +68,9 @@ public class Player {
         }
     }
 
-    public void HealthLvl(String input){
+    public void HealthLvl(String input) {
         if (Objects.equals(input, "0")) {
-            System.out.println("You are at "+health.toString()+" HP.");
+            System.out.println("You are at " + health.toString() + " HP.");
             if (health == 3) {
                 System.out.println("You are feeling healthy.");
             } else if (health == 2) {
@@ -79,37 +78,38 @@ public class Player {
             } else if (health == 1) {
                 System.out.println("Your vision clouds. You will not last much longer.");
             }
-            //while this isn't a hack, we could implement it in a cooler way when 
-            //we realise what the fuck a hashtable is
+            // we now know what a hashtable is, might add a few different descriptors for
+            // each health and hunger value
 
         }
     }
 
-    public void WorldLevel(String input){
+    public void WorldLevel(String input) {
         if (Objects.equals(input, "8")) {
-            System.out.println(biomeCount.toString()+"-"+roomCount.toString());
+            System.out.println(biomeCount.toString() + "-" + roomCount.toString());
         }
     }
 
-    public void FoodLvl(String input){
+    public void FoodLvl(String input) {
         if (Objects.equals(input, "9")) {
-            System.out.println("You have "+food.toString()+" food left.");
+            System.out.println("You have " + food.toString() + " food left.");
             if (food >= 25) {
                 System.out.println("Hunger pangs.");
             } else if (food <= 75) {
                 System.out.println("You are feeling well fed.");
             }
-            //see the comment on lines 85-86
+            // see the comment on line 82
         }
     }
 
     public void Wait() {
         if (this.health < 3) {
-            this.health+= 1;
+            this.health += 1;
             System.out.println("After taking a well deserved break, you feel rejuvenated and enjoy a burst of energy.");
-            System.out.println("You now have " +health.toString()+ " health remaining.");
+            System.out.println("You now have " + health.toString() + " health remaining.");
         } else {
-            System.out.println("Although you have rested, you feel no different. It's as if you were already full of vitality.");
+            System.out.println(
+                    "Although you have rested, you feel no different. It's as if you were already full of vitality.");
             System.out.println("You still have 3 health remaining");
         }
     }
