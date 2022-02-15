f736c6975ea3afd88b70caa6cf6c05cffc896ef8
Andrew Barrett <andrewbarrett148@gmail.com>
2022-02-15 15:44:45 +0000

    Bones of puzzle.java started

--- src/main/java/com/CambrianAdventure/app/exploration/Scenarios/Puzzle.java
+++ src/main/java/com/CambrianAdventure/app/exploration/Scenarios/Puzzle.java
@@ -3,7 +3,31 @@ package com.CambrianAdventure.app.exploration.Scenarios;
 import com.CambrianAdventure.app.exploration.Scenario;
 
 public class Puzzle extends Scenario {
+    
+    public boolean p1Complete = false;
+    public boolean p2Complete = false;
+    public boolean p3Complete = false;
+
+    public Integer p1Path = 0;
+    public Integer p2Path = 0;
+    
     public Puzzle(){
         super(1, "Puzzle");
+    }
+
+    public void PuzzlePaths(){
+        
+        if(p3Complete){
+            PuzzleRun();
+            completed = true;
+        }
+        if(p2Complete){
+            PuzzleRun();
+        }
+        if(p1Complete){
+            PuzzleRun();
+        }
+    }
+    public void PuzzleRun(){
     }
 }
